e7f574534fbbbb7038acab12ada1ad8630c8dfe3
Anumakonda Vyshnav Kumar <128172678+Vyshnav31@users.noreply.github.com>
2023-10-16 23:34:35 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,117 @@
+const logotext = "A.Vyshnav Kumar";
+const meta = {
+    title: "A.Vyshnav Kumar",
+    description: "I’m A.Vyshnav Kumar Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m A.Vyshnav Kumar",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I am a Full Stack Developer",
+    },
+    description: "My name is Vyshnav Kumar and i'm 3rd year student pursuing B.tech, CSE(core ) from VIT Chennai ,i'm interested in problem solving ,Web development and UI/UX.I'm quite good in solving coding questions and improve my skills day to day which helps me in real time world and i have a good communicational skills which helps me in gathering of information",
+    your_img_url: "https://www.itprotoday.com/sites/itprotoday.com/files/styles/article_featured_retina/public/programming%20evolution.jpg?itok=WTj9-yNz",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "My name is Vyshnav Kumar and i'm 3rd year student pursuing B.tech, CSE(core ) from VIT Chennai ,i'm interested in problem solving ,Web development and UI/UX.I'm quite good in solving coding questions and improve my skills day to day which helps me in real time world and i have a good communicational skills which helps me in gathering of information",
+};
+const worktimeline = [{
+        jobtitle: "Data Science intern ",
+        where: "Capabl",
+        date: "2023",
+    },
+    {
+        jobtitle: "Technical Member",
+        where: "Open Source Club",
+        date: "2023",
+    },
+  
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "C++",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "HTML/CSS",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "MongoDB",
+        value: 60,
+    },
+    
+    {
+        name:"NodeJS",
+        value:60,
+    },
+
+];
+
+
+
+const dataportfolio = [{
+        img: "https://cdn-icons-png.flaticon.com/512/7477/7477790.png",
+        description: "Weather application which show real time information",
+        link: "https://weather-application-sage-nu.vercel.app/",
+    },
+    {
+        img: "https://www.joomshaper.com/images/2018/09/24/parallax-effect-banner.gif",
+        description: "Parallax Scroll ",
+        link: "https://vyshnav31.github.io/Parallax-scrolling/",
+    },
+    {
+        img: "https://developer.redis.com/assets/images/chatapp_homepage-e18893a7fe583a1dbda4c15347ee9d52.png",
+        description: "Real-time chat application",
+        link: "#",
+    },
+    {
+        img: "https://www.featuretools.com/wp-content/uploads/2018/12/taxi-min-1024x1024.jpg",
+        description: "Predict Taxi Trip",
+        link: "https://github.com/Vyshnav31/Predict_Taxi_Trip",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vyshnavkumar.a2021@vitstudent.ac.in",
+    YOUR_FONE: "6302577402",
+    description: "Message me",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "#",
+    facebook: "#",
+    linkedin: "#",
+    twitter: "#",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
